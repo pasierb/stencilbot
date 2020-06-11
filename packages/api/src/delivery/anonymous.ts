e10998f852812b813132bed687fc3f0929df79dd
Michal Pasierbski <michal.pasierbski@ef.com>
2020-06-11 15:56:50 +0200

    Cleanup api

--- packages/api/src/delivery/anonymous.ts
+++ packages/api/src/delivery/anonymous.ts
@@ -1,63 +1,15 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent } from 'aws-lambda'
-import { createCanvas, Canvas, registerFont } from 'canvas';
-import { Layer, LayerInit } from '@stencilbot/renderer';
+import { APIGatewayProxyHandler } from 'aws-lambda'
+import { Project } from '@stencilbot/renderer';
 import { ServerRenderer } from '../serverRenderer';
 import { registerDefaultFonts } from '../fonts';
 
-interface ParseEventResult {
-  width: number
-  height: number
-  layers: Layer[]
-}
-
 const renderer = new ServerRenderer();
-const layerParamPattern = /^l\[(?<order>\d+)\](?<attr>\w+)$/;
-
-function parseEvent(event: APIGatewayProxyEvent): ParseEventResult {
-  const q = event.queryStringParameters;
-  const width = +q['w']
-  const height = +q['h']
-
-  const layers = Object.entries(q).reduce<LayerInit[]>((acc, [key, value]) => {
-    const match = key.match(layerParamPattern);
-
-    if (match) {
-      const order = +match.groups.order;
-      const attr = match.groups.attr;
-      const l: LayerInit = acc[order] || { order };
-
-      l[attr] = value;
-      acc[order] = l;
-    }
-
-    return acc;
-  }, []).map(init => new Layer(init));
-
-  return {
-    width,
-    height,
-    layers
-  };
-}
 
 registerDefaultFonts();
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-  const { width, height, layers } = parseEvent(event);
-  const base = createCanvas(width, height);
-  const ctx = base.getContext('2d');
-
-  const canvasLayers = await Promise.all(layers.map(layer => new Promise<Canvas>((resolve, reject) => {
-    const canvas = createCanvas(width, height);
-
-    renderer.render(canvas as unknown as HTMLCanvasElement, layer)
-      .then(() => resolve(canvas))
-      .catch(reject);
-  })));
-
-  canvasLayers.forEach((canvas) => {
-    ctx.drawImage(canvas, 0, 0, width, height);
-  });
+  const project = Project.fromSearchParams(event.queryStringParameters);
+  const base = await renderer.renderProject(project);
 
   return {
     statusCode: 200,
--- packages/api/src/serverRenderer.ts
+++ packages/api/src/serverRenderer.ts
@@ -1,5 +1,5 @@
-import { Renderer } from '@stencilbot/renderer';
-import { loadImage } from 'canvas';
+import { Renderer, Project } from '@stencilbot/renderer';
+import { createCanvas, loadImage, Canvas } from 'canvas';
 import fetch from 'node-fetch';
 
 export class ServerRenderer extends Renderer {
@@ -7,5 +7,24 @@ export class ServerRenderer extends Renderer {
     return fetch(uri)
       .then(res => res.buffer())
       .then(buf => loadImage(buf) as unknown as CanvasImageSource)
+  }
+
+  async renderProject(project: Project): Promise<Canvas> {
+    const base = createCanvas(project.width, project.height);
+    const ctx = base.getContext('2d');
+
+    const canvasLayers = await Promise.all(project.layers.map(layer => new Promise<Canvas>((resolve, reject) => {
+      const canvas = createCanvas(project.width, project.height);
+
+      this.render(canvas as unknown as HTMLCanvasElement, layer)
+        .then(() => resolve(canvas))
+        .catch(reject);
+    })));
+
+    canvasLayers.forEach((canvas) => {
+      ctx.drawImage(canvas, 0, 0, project.width, project.height);
+    });
+
+    return base;
   }
 }
