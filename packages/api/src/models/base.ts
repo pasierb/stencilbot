697bc9a2b6c3b35cf27075d49da0f1525fd88354
Michal Pasierbski <michal.pasierbski@ef.com>
2020-06-11 15:55:45 +0200

    Remove unused dynamodb models

--- packages/api/src/models/base.ts
+++
@@ -1,9 +0,0 @@
-import { DynamoDB } from 'aws-sdk';
-
-export abstract class Base {
-  abstract async save(db: DynamoDB)
-
-  abstract toJSON(): object
-
-  abstract toDynamoDBItem(): DynamoDB.AttributeMap
-}
