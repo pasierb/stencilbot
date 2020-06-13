3ac6f6baf5b55e81070719af85379d5e9f642bd9
Michal Pasierbski <michal.pasierbski@ef.com>
2020-06-14 00:15:52 +0200

    Add homepage styling

--- packages/dashboard/src/routes/HomeRoute.tsx
+++ packages/dashboard/src/routes/HomeRoute.tsx
@@ -1,26 +1,37 @@
 import React, { FunctionComponent } from 'react';
-import { RouteComponentProps, navigate } from '@reach/router';
-import { Form, Input, Layout, Button, Row, Col } from 'antd';
+import { RouteComponentProps, Link, navigate } from '@reach/router';
+import { Form, Input, Layout, Button, Row, Col, Card, Typography } from 'antd';
 import { Store } from 'antd/lib/form/interface';
-import { TopBar } from '../components/TopBar';
+import { GithubOutlined, EditOutlined } from '@ant-design/icons';
+import samples from '../samples.json';
+
+import style from './HomeRoute.module.css';
 
 interface HomeFormStore extends Store {
   url: string
 }
 
+function editPath(uri: string) {
+  const url = new URL(uri);
+
+  return `/projects/edit${url.search}`;
+}
+
 export const HomeRoute: FunctionComponent<RouteComponentProps> = () => {
   const [form] = Form.useForm();
+
+  const goToEdit = (uri: string) => {
+    try {
+      navigate(editPath(uri));
+    } catch(e) {
+      console.log(e);
+    }
+  }
 
   const handleEdit = (values: Store) => {
     const urlString = values['url'] as string
 
-    try {
-      const url = new URL(urlString);
-
-      navigate(`/projects/edit${url.search}`)
-    } catch(e) {
-      console.log(e);
-    }
+    goToEdit(urlString);
   }
 
   const handleNew = () => {
@@ -28,27 +39,68 @@ export const HomeRoute: FunctionComponent<RouteComponentProps> = () => {
   }
 
   return (
-    <Layout>
+    <Layout className={style.HomeRoute}>
       <Layout.Content>
-        <Row justify="space-around">
-          <Col span={10}>
-            <Button onClick={handleNew}>Create new</Button>
+        <Row justify="center">
+          <Col>
+            <Typography.Title>Stencilbot</Typography.Title>
+          </Col>
+        </Row>
+
+        <Row justify="center" gutter={10}>
+          <Col lg={6}>
+            <Card>
+              <Button onClick={handleNew}>Create new</Button>
+            </Card>
           </Col>
 
-          <Col span={10}>
-            <Form form={form} size="large" onFinish={handleEdit}>
-              <Form.Item name="url">
-                <Input placeholder="Edit" />
-              </Form.Item>
-              <Form.Item>
-                <Button htmlType="submit">Submit</Button>
-              </Form.Item>
-            </Form>
+          <Col lg={6}>
+            <Card>
+              <Form form={form} size="large" onFinish={handleEdit}>
+                <Form.Item name="url">
+                  <Input placeholder="Edit" />
+                </Form.Item>
+                <Form.Item>
+                  <Button htmlType="submit">Submit</Button>
+                </Form.Item>
+              </Form>
+            </Card>
           </Col>
+        </Row>
+
+        <Row justify="center" gutter={20}>
+          {samples.map((sample, i) => (
+            <Col lg={3} sm={5} xs={12} key={i}>
+              <Card
+                onClick={() => goToEdit(sample.url)}
+                hoverable
+                cover={<img src={sample.url} alt={sample.title} />}
+                actions={[
+                  <Link to={editPath(sample.url)}><EditOutlined /></Link>
+                ]}
+              >
+                <Card.Meta
+                  title={sample.title}
+                  description={sample.description}
+                />
+              </Card>
+            </Col>
+          ))}
         </Row>
       </Layout.Content>
       <Layout.Footer>
-        asd
+        <ul className={style.footerLinks}>
+          <li>
+            <a href="https://github.com/pasierb/stencilbot">
+              <GithubOutlined /> View the code
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/pasierb/stencilbot/issues">
+              Report a bug
+            </a>
+          </li>
+        </ul>
       </Layout.Footer>
     </Layout>
   );
