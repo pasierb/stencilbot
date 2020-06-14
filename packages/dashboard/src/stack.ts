cc02576a70c23b07d0ff0d6e1fe902f3b0f5323f
Michal Pasierbski <michal.pasierbski@ef.com>
2020-06-14 23:54:10 +0200

    Add dashboard stack

---
+++ packages/dashboard/src/stack.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { Stack } from '@aws-cdk/core';
+import { Bucket } from '@aws-cdk/aws-s3';
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
+
+export class StencilbotDashboardStack extends Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const bucket = new Bucket(this, 'stencilbot-dashboard-static');
+
+    new BucketDeployment(this, 'stencilbot-dashboard-static-deployment', {
+      destinationBucket: bucket,
+      sources: [
+        Source.asset(path.join(__dirname, '../out'))
+      ]
+    })
+  }
+}
