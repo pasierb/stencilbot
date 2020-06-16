260beb5b63905754ed067d4163a3be91e6c467dd
Michal Pasierbski <michal.pasierbski@ef.com>
2020-06-16 10:08:48 +0200

    Add cache-control header

--- packages/api/src/delivery/anonymous.ts
+++ packages/api/src/delivery/anonymous.ts
@@ -1,11 +1,8 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
 import { Project } from '@stencilbot/renderer';
 import { ServerRenderer } from '../serverRenderer';
-import { registerDefaultFonts } from '../fonts';
 
 const renderer = new ServerRenderer();
-
-registerDefaultFonts();
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   console.log(event.queryStringParameters);
@@ -20,7 +17,8 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     body: base.toBuffer().toString('base64'),
     isBase64Encoded: true,
     headers: {
-      'Content-Type': 'image/png'
+      'Content-Type': 'image/png',
+      'Cache-Control': 'max-age=31536000'
     }
   };
 }
