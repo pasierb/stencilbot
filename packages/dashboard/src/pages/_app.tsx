d50f1cad3772381a4d0a213a73ba95e73778364b
Michal Pasierbski <michal.pasierbski@ef.com>
2020-06-18 14:26:31 +0200

    Add sentry

--- packages/dashboard/src/pages/_app.tsx
+++ packages/dashboard/src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { AppProps } from 'next/app'
+import * as Sentry from '@sentry/browser';
 
 import 'antd/dist/antd.css';
 import '../index.css';
+
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  Sentry.init({ dsn: process.env.NEXT_PUBLIC_SENTRY_DSN });
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
