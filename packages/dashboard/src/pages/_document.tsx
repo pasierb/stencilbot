fe6ec3e07daab32ac70bffc104266b22fd7f7ff7
Michal Pasierbski <michal.pasierbski@ef.com>
2020-06-18 09:04:31 +0200

    Add GA tracking code

--- packages/dashboard/src/pages/_document.tsx
+++ packages/dashboard/src/pages/_document.tsx
@@ -13,6 +13,15 @@ class MyDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Saira+Stencil+One&display=swap" rel="stylesheet" />
           <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_CODE}`}></script>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_CODE}');
+            `
+          }} />
         </Head>
         <body>
           <Main />
